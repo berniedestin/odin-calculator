c16947a2e1156cf9fef0c5943dc6c14c1ebea545
Destin <berniedestin7@gmail.com>
2023-03-03 11:51:10 -0500

    Finalize operator function with error catching

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
 // calculator
 
 // create variables for DOM items
+// operate function per Odin
+function operate(symOperator,a,b){
+    // Error catch symOperator not a string
+    if (typeof(symOperator) != "string") return "ERROR"
+    // Error catch a not number
+    if (typeof(a) != 'number') {
+        if (typeof(a) == 'string' ) {
+            a = +a
+            if ( isNaN(a) )  return "ERROR"
+        }
+    }
+    // Error catch b not number
+    if (typeof(b) != 'number') {
+        if (typeof(b) == 'string' ) {
+            b = +b
+            if ( isNaN(b) )  return "ERROR"
+        }
+    }    
+    // runs correct function bsed on operator
+    if ( symOperator === "+" ) {
+        return add(a,b)
+    } else if ( symOperator === "-" ){
+        return subtract(a,b)
+    } else if ( symOperator === "*"){
+        return mult(a,b)
+    } else if ( symOperator === "/" ) {
+        return divide(a,b)
+    }
+}
 
-// add function
+// addition function
 function add(){
     if (arguments.length < 2) return "ERROR"
     let sum = 0
