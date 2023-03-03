18cf4de60915c60fb81e298480301e187e73b142
Destin <berniedestin7@gmail.com>
2023-03-03 15:03:14 -0500

    Add snarky comment on /0 and allow user to continue operating on answers

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // calculator
 
+// YET TO DO LIST
+// - snarky message if divide by 0
+// - string together answers
+
 // create variables for DOM items
 
 const subHead = document.querySelector('.sub-head')
@@ -7,10 +11,6 @@ const calcShow = document.querySelector('#calc-show')
 const calcOutput = document.querySelector('#calc-output')
 const buttons = document.querySelectorAll('.btn-basic')
 buttons.forEach( button => button.addEventListener('click', assignValue))
-
-// subHead.textContent = ''
-// calcShow.textContent = ''
-// calcOutput.textContent = ''
 
 
 // this object is for actually holding the values
@@ -23,11 +23,33 @@ function assignValue(){
     // erase error message
     subHead.textContent = ''
     // checks which button was pressed and acts 
+    
     if ( buttonName == 'op') {
         // check if can run operator
         if ( "num1" in bgMath && "operator" in bgMath && "num2" in bgMath) {
+            // update output
+            showMath(calcOutput.textContent)
+
             bgMath.ans = operate(bgMath.operator,bgMath.num1,bgMath.num2)
-            showMath()
+            if (bgMath.ans == Infinity) {
+                clear()
+                subHead.textContent = "Snarky snark YARR!"
+            } else {
+            updateOutput(bgMath.ans)
+            }
+            // showMath()
+        } else if ( "num1" in bgMath && "operator" in bgMath && "num2" in bgMath && "ans" in bgMath ) {
+            // update output
+            showMath(calcOutput.textContent)
+            
+            bgMath.ans = operate(bgMath.operator,bgMath.num1,bgMath.num2)
+            if (bgMath.ans == Infinity) {
+                clear()
+                subHead.textContent = "Snarky snark YARR!"
+            } else {
+            updateOutput(bgMath.ans)
+            }
+
         }
     } else if ( buttonName == 'clear') {
         clear()
@@ -35,44 +57,52 @@ function assignValue(){
                 buttonName == '-' ||
                 buttonName == '*' ||
                 buttonName == '/' ) {
-        updateOutput("")
-        // checks to erase old data
-        if ( "ans" in bgMath) clear();
+        if ( calcOutput.textContent != bgMath.ans)showMath(calcOutput.textContent);
+        // updateOutput("")
+        // checks to erase old data-------------------------------
+        // if ( "ans" in bgMath) clear();
         if ( !("num1" in bgMath) && !("num2" in bgMath)) {
             // no numbers entered yet
             subHead.textContent = 'You need to enter a number first';
-        } else if ( "operator" in bgMath ) {
-            // already operator
-            subHead.textContent = 'You already entered an operator'
-        } else {
+        } else if ( "operator" in bgMath && "ans" in bgMath ) {
+            // adds operator if ans exists
+            bgMath.num1 = bgMath.ans
+            bgMath.num2 = ''
             bgMath.operator = buttonName
-            showMath()            
+            showMath(buttonName)
+        } 
+        // else if ( "operator" in bgMath ) {
+        //     // already operator
+        //     subHead.textContent = 'You already entered an operator'
+        // } 
+        else {
+            bgMath.operator = buttonName
+            showMath(buttonName)
         }
     } else {
         // checks to erase old data
-        if ( "ans" in bgMath) clear();
+        // if ( "ans" in bgMath) clear();
         // runs normal numbers
         if ( !("num1" in bgMath)) {
             // no num1, so fill in num1
             bgMath.num1 = buttonName
-            console.log("first if")
             updateOutput(buttonName)
-            showMath()
+            // showMath(buttonName)
         } else if ( "num1" in bgMath && !("operator" in bgMath)){
             // num1 needs to be appended, no operator yet
             bgMath.num1 = `${bgMath.num1}${buttonName}`
             updateOutput(bgMath.num1)
-            showMath()
+            // showMath()
         } else if ( !("num2" in bgMath) && "operator" in bgMath) {
             // operator, but no num2, fill in num2
             bgMath.num2 = buttonName
             updateOutput(buttonName)
-            showMath()
+            // showMath()
         } else if ( ("num2" in bgMath) && "operator" in bgMath) {
             // operator and num2, append num2
             bgMath.num2 = `${bgMath.num2}${buttonName}`
             updateOutput(bgMath.num2)
-            showMath()
+            // showMath()
         } else if ( !("num2" in bgMath) && !("operator" in bgMath)) {
             subHead.textContent = "You need to enter an operator"
         } else {
@@ -81,16 +111,17 @@ function assignValue(){
     }
 }
 
-function showMath() {
-    if ( "num1" in bgMath && "operator" in bgMath && "num2" in bgMath && "ans" in bgMath )  {
-        calcShow.textContent = `${bgMath.num1} ${bgMath.operator} ${bgMath.num2}`
-        calcOutput.textContent = ''
-        calcOutput.textContent = `${bgMath.ans}`
-    } else if ( "num1" in bgMath && "operator" in bgMath) {
-        calcShow.textContent = `${bgMath.num1} ${bgMath.operator}`
-    } else {
-        calcShow.textContent = ''
-    }    
+function showMath(str) {
+    calcShow.textContent = calcShow.textContent.concat(" ", str)
+    // if ( "num1" in bgMath && "operator" in bgMath && "num2" in bgMath && "ans" in bgMath )  {
+    //     calcShow.textContent = `${bgMath.num1} ${bgMath.operator} ${bgMath.num2}`
+    //     calcOutput.textContent = ''
+    //     calcOutput.textContent = `${bgMath.ans}`
+    // } else if ( "num1" in bgMath && "operator" in bgMath) {
+    //     calcShow.textContent = `${bgMath.num1} ${bgMath.operator}`
+    // } else {
+    //     calcShow.textContent = ''
+    // }    
 }
 
 function updateOutput(num) {
